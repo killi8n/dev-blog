89af5365bd400404e30ced0d58c9424bd8547200
yungblud <dom@zigbang.com>
2021-01-16 20:00:43 +0900

    fix: naming

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Layout from '../components/_Layout';
+import SEO from '../components/_SEO';
 
 const NotFoundPage = () => (
   <Layout>
--- src/templates/post-detail.jsx
+++ src/templates/post-detail.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 import { graphql } from 'gatsby';
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/_Layout';
 import PostDetail from '../components/postDetail';
-import SEO from '../components/SEO';
+import SEO from '../components/_SEO';
 import LinkedPostList from '../components/linkedPostList';
 
 const PostDetailTemplate = (props) => {
